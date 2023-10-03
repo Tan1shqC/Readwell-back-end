5a8da000aa24e347a796160d1c3666b371b458fd
Tan1shqC <tanishqchoudhary1@gmail.com>
2023-10-03 13:00:08 +0530

    allowed font-end domain in cors

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,8 +1,6 @@
 const allowedOrigins = [
     "http://localhost:3000",
-    "https://www.bookmanagementsystem.com",
-    "https://bookmanagementsystem.com",
-    "https://tan1shqc.github.io/Readwell-front-end/"
+    "https://readwell.onrender.com",
 ];
 
 module.exports = allowedOrigins;
